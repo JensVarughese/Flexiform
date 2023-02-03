cc465b8d429c86cf957d95cdff1006625604fbb2
Tahseen <tahseen.intesar@gmail.com>
2023-02-03 15:31:06 -0700

    fixed rotating function (no more zoom out bug)

--- Capstone Concept/Assets/Scripts/CameraController.cs
+++ Capstone Concept/Assets/Scripts/CameraController.cs
@@ -5,17 +5,20 @@ public class CameraController : MonoBehaviour
 {
     public GameObject handObj;
     private Transform cameraTransform;
+    private Transform parentTransform;
     private Vector3 handCenter;
-    private float distance;
     private Vector3? mousePos;
+    private float distance;
 
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = gameObject.GetComponent<Transform>();
+        parentTransform = cameraTransform.parent;
         var handTransform = handObj.transform.GetChild(0);
         var mesh = handTransform.GetComponent<MeshFilter>().mesh;
         handCenter = GetCenter(mesh);
+        parentTransform.position = handCenter;
         cameraTransform.LookAt(handCenter);
         distance = Vector3.Distance(transform.position, handCenter);
     }
@@ -23,11 +26,29 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        //Fire2 = Right Click
         if (Input.GetButton("Fire2"))
+        {
             RotateCamera();
+        }
         else if (Input.GetButtonUp("Fire2"))
+        {
             mousePos = null;
+        }
 
+        /*
+
+            Camera is moving to new position, then rotating to point at center.
+            Rotating too fast before the calculation/updating causes the zoom-out effect.
+
+            IDEAS:
+
+            - 
+
+            - 
+        */
+
+        //allows zooming
         ZoomCamera(Input.mouseScrollDelta.y);
     }
 
@@ -52,21 +73,27 @@ public class CameraController : MonoBehaviour
             return;
         }
 
+        // mouse position after the rotation is done
         var newMousePos = Input.mousePosition;
+
+        //calculate mouse position differences and angle to rotate
         var diffX = (mousePos?.x ?? 1) - newMousePos.x;
         var diffY = (mousePos?.y ?? 1) - newMousePos.y;
         var xRot = transform.eulerAngles.x;
-        var rotationScale = distance / 200;
 
-        transform.Translate(Vector3.right * diffX * rotationScale);
-
+        //edge cases for boundaries
         var edgeCase1 = isBet(xRot, 70, 180) && diffY < 0;
         var edgeCase2 = isBet(xRot, 180, 290) && diffY > 0;
-        if (isBet(xRot, -1, 70) || isBet(xRot, 290, 360) || edgeCase1 || edgeCase2) 
-            transform.Translate(Vector3.up * diffY * rotationScale);
 
+        if (isBet(xRot, -1, 70) || isBet(xRot, 290, 360) || edgeCase1 || edgeCase2) 
+        {
+            parentTransform.Rotate(-diffY / 2.0f, -diffX / 2.0f, 0.0f, Space.Self);
+        }
+
+        //once at the new position, look at center
         cameraTransform.LookAt(handCenter);
 
+        //set mouse position
         mousePos = newMousePos;
     }
 
